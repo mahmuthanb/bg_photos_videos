2e705622536baa4a779dd9fd98b6540b94d40430
Mahmuthan <mahmuthanb@gmail.com>
2022-03-28 01:32:16 +0300

    ImageDetail screen changes

--- lib/view/image_detail_screen.dart
+++ lib/view/image_detail_screen.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:bg_photos_videos/constants/style_guide.dart';
 import 'package:bg_photos_videos/data/model/image_model.dart';
+import 'package:bg_photos_videos/view/widgets/custom_cached_network_image.dart';
 import 'package:bg_photos_videos/view/widgets/custom_webview.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
@@ -27,7 +28,6 @@ class ImageDetail extends StatelessWidget {
             fontSize: 18,
           ),
         ),
-        // leadingWidth: ,
         centerTitle: true,
         toolbarHeight: kToolbarHeight,
         leadingWidth: kToolbarHeight,
@@ -39,19 +39,12 @@ class ImageDetail extends StatelessWidget {
           builder: (BuildContext context) {
             return Container(
               margin: const EdgeInsets.all(8),
-              // decoration: const BoxDecoration(
-              //   shape: BoxShape.circle,
-              //   color: Colors.white70,
-              // ),
               child: IconButton(
                 icon: const Icon(
                   Icons.arrow_back,
                   color: Colors.white70,
-                  // color: Color(
-                  //     int.parse("0xFF${image.avgColor.replaceAll("#", "")}")),
                 ),
                 onPressed: () {
-                  // Scaffold.of(context).openDrawer();
                   Navigator.pop(context);
                 },
                 tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
@@ -87,34 +80,114 @@ class ImageDetail extends StatelessWidget {
                 // Optional Image Buttons
                 String avgColor = image.avgColor.replaceAll("#", "");
                 return GestureDetector(
-                  onTap: () => showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                            // title: const Text("Image Box"),
-                            insetPadding: EdgeInsets.zero,
-                            contentPadding: EdgeInsets.zero,
-                            content: BackdropFilter(
-                              filter: ImageFilter.blur(
-                                sigmaX: 4.0,
-                                sigmaY: 4.0,
-                              ),
-                              child: SizedBox(
-                                width: size.width * .75,
-                                height: size.height * .75,
-                                child: GestureDetector(
-                                  onTap: () {
-                                    Navigator.pop(context);
-                                  },
-                                  child: PhotoView(
-                                    imageProvider: CachedNetworkImageProvider(
-                                      image.src.values.elementAt(index),
-                                    ),
+                  onTap: () => showModalBottomSheet(
+                    context: context,
+                    barrierColor: Colors.black87,
+                    isScrollControlled: true,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(size.width * .025),
+                      ),
+                    ),
+                    builder: (context) => Container(
+                      height: size.height * .8,
+                      // color: Colors.green,
+                      padding: const EdgeInsets.symmetric(
+                          // vertical: size.height * .075,
+                          ),
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(size.width * .025),
+                            child: CustomCachedNetworkImage(
+                              image.src.values.elementAt(index),
+                              width: size.width,
+                              height: size.height * .6,
+                            ),
+                          ),
+                          const Text("ABOUT PHOTOGRAPHER"),
+                          Divider(
+                            height: size.height * .025,
+                            color: Color(int.parse(
+                                "0xFF${image.avgColor.replaceAll("#", "")}")),
+                            indent: size.width * .1,
+                            endIndent: size.width * .1,
+                            thickness: 1,
+                          ),
+                          GestureDetector(
+                            onTap: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (BuildContext context) =>
+                                        InAppWebview(
+                                          image.photographerUrl,
+                                          Color(int.parse(
+                                              "0xFF${image.avgColor.replaceAll("#", "")}")),
+                                        ))),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  // color: Colors.red,
+                                  width: size.width * .2,
+                                  child: Icon(
+                                    Icons.photo_camera_front_rounded,
+                                    size: 50,
+                                    color: Color(int.parse(
+                                        "0xFF${image.avgColor.replaceAll("#", "")}")),
                                   ),
                                 ),
-                              ),
-                            ));
-                      }),
+                                Container(
+                                  width: size.width * .7,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(image.photographer),
+                                      Text(image.photographerUrl,
+                                          maxLines: 2,
+                                          overflow: TextOverflow.ellipsis),
+                                      // Text(image.photographerId.toString()),
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // showDialog(
+                  //     context: context,
+                  //     builder: (context) {
+                  //       return AlertDialog(
+                  //         insetPadding: EdgeInsets.zero,
+                  //         contentPadding: EdgeInsets.zero,
+                  //         content: BackdropFilter(
+                  //           filter: ImageFilter.blur(
+                  //             sigmaX: 4.0,
+                  //             sigmaY: 4.0,
+                  //           ),
+                  //           child: SizedBox(
+                  //             width: size.width * .75,
+                  //             // height: size.height * .75,
+                  //             child: GestureDetector(
+                  //               onTap: () {
+                  //                 Navigator.of(context, rootNavigator: true)
+                  //                     .pop();
+                  //                 // Navigator.pop(context);
+                  //               },
+                  //               child: PhotoView(
+                  //                 imageProvider: CachedNetworkImageProvider(
+                  //                   image.src.values.elementAt(index),
+                  //                 ),
+                  //               ),
+                  //             ),
+                  //           ),
+                  //         ),
+                  //       );
+                  //     }),
                   child: Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(kGlobalRadius),
