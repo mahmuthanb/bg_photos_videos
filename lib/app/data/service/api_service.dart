db70e1764be8cf8f1ba620b6b81c801431607939
Mahmuthan <mahmuthanb@gmail.com>
2023-02-19 23:56:29 +0300

    api_service.dart

--- lib/app/data/service/api_service.dart
+++ lib/app/data/service/api_service.dart
@@ -6,21 +6,7 @@ part "api_service.g.dart";
 @RestApi()
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
-  // /captcha2 // şimdilik Kullanılmıyor
-
-  // /api/auth/getKVKKConsent
-  // /api/auth/saveKVKKConsent
-  // /api/check/checkForFoodStoreButon
-  // /api/auth/kvkkHealth
-  // /api/auth/getKVKKHealthSurvey
-  // /api/auth/saveKVKKHealthSurvey
 
   @POST("/v1/curated")
-  Future<List<ImageModel>> fetchImages(
-    @Field() int allowToCommunicate,
-    @Field() String deviceId,
-    @Field() String emailOrIdentity,
-    @Field() String password, {
-    @Field() String? phoneNumber,
-  });
+  Future<List<ImageModel>> fetchImages();
 }
