44d6dfb7396d0a8377023aeaf4cf6bdb1a05a3d2
Mahmuthan <mahmuthanb@gmail.com>
2023-02-22 23:43:08 +0300

    api service parameters added to di

--- lib/core/di/module.dart
+++ lib/core/di/module.dart
@@ -1,3 +1,4 @@
+import 'package:bg_photos_videos/app/data/api_provider.dart';
 import 'package:bg_photos_videos/core/source/app_config.dart';
 import 'package:bg_photos_videos/core/di/locator.dart';
 import 'package:dio/dio.dart';
@@ -10,11 +11,11 @@ abstract class AppModule {
   Dio get injectRetrofit {
     Dio dio = Dio(
       BaseOptions(
-        baseUrl: getIt<AppConfig>().baseUrl,
-        connectTimeout: const Duration(milliseconds: 10000),
-        receiveTimeout: const Duration(milliseconds: 10000),
-        sendTimeout: const Duration(milliseconds: 10000),
-      ),
+          baseUrl: getIt<AppConfig>().baseUrl,
+          connectTimeout: const Duration(milliseconds: 10000),
+          receiveTimeout: const Duration(milliseconds: 10000),
+          sendTimeout: const Duration(milliseconds: 10000),
+          headers: {"Authorization": getIt<AppConfig>().apiKey}),
     );
     if (kDebugMode) {
       dio.interceptors.add(PrettyDioLogger(
--- lib/core/source/app_config.dart
+++ lib/core/source/app_config.dart
@@ -1,9 +1,11 @@
 // Package imports:
+import 'package:bg_photos_videos/app/data/api_provider.dart';
 import 'package:injectable/injectable.dart';
 
 abstract class AppConfig {
   String get appName;
   String get baseUrl;
+  String get apiKey;
 }
 
 @Environment(Environment.prod)
@@ -13,6 +15,8 @@ class AppConfigProd implements AppConfig {
   String get baseUrl => "https://api.pexels.com";
   @override
   String get appName => "BG Photos & Videos";
+  @override
+  String get apiKey => pexelsApi;
 }
 
 @Environment(Environment.dev)
@@ -21,7 +25,8 @@ class AppConfigProd implements AppConfig {
 class DevConfig implements AppConfig {
   @override
   String get baseUrl => "https://api.pexels.com";
-
   @override
   String get appName => "[DEV] BG Photos & Photos";
+  @override
+  String get apiKey => pexelsApi;
 }
